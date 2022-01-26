e0db9100eec3fa787c4f3e33f5dc8bdf633563e5
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-26 11:50:52 +0300

    add challange

--- script.js
+++ script.js
@@ -58,3 +58,25 @@ function maxContiguousSum(arr) {
 
 console.log(maxContiguousSum([3, -4, 8, 7, -10, 19, -3]));
 console.log(maxContiguousSum([-8, -10, -12, -2, -3, 5]));
+
+// 83. Maximum subarray sum -5kyu
+/* The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+// should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray. */
+
+var maxSequence = function (arr) {
+    let max = arr[0] || 0;
+
+    let total = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+        if (total <= 0) total = 0;
+        if (total > max) max = total;
+    }
+    return max;
+};
