f6c929ea4696a98c9ccb4cacb2ad850f1178c306
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-27 11:51:56 +0300

    add challange

--- script.js
+++ script.js
@@ -80,3 +80,59 @@ var maxSequence = function (arr) {
     }
     return max;
 };
+
+// 84. Maximum Subarray Sum II - 5kyu
+
+/* In Maximum subarray sum challenge you had to give the maximum value of the elements of all the subarrays.
+
+In this challenge, we have a similar task but you have to find the sub-array or sub-arrays having this maximum value for their corresponding sums of elements. The wanted function: Python and Ruby: find_subarr_maxsum()// Javascript: findSubarrMaxSum()
+
+find_subarr_maxsum([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == [[4, -1, 2, 1], 6]
+If in the solution we have two or more arrays having the maximum sum value, the result will have an array of arrays in the corresponding order of the array, from left to right.
+
+find_subarr_maxsum([4, -1, 2, 1, -40, 1, 2, -1, 4]) == [[[4, -1, 2, 1], [1, 2, -1, 4]], 6]  # From left to right [4, -1, 2, 1] appears in the array before than [1, 2, -1, 4].
+If the array does not have any sub-array with a positive sum of its terms, the function will return [[], 0]. */
+
+function find_subarr_maxsum(arr) {
+    let max = arr[0] || 0;
+    let subArr = [],
+        totalArr = [];
+    let total = 0;
+    let subIndxstart = 0,
+        subIndxend = 0;
+
+    const maxObj = {};
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+
+        if (total <= 0) {
+            total = 0;
+            subIndxstart = i + 1;
+        }
+        if (total >= max && total !== 0) {
+            max = total;
+            subIndxend = i;
+            maxObj[max]
+                ? maxObj[max].push([subIndxstart, subIndxend])
+                : (maxObj[max] = [[subIndxstart, subIndxend]]);
+
+            // if (!maxObj[max]) {
+            //     maxObj[max] = [[subIndxstart, subIndxend]];
+            // } else {
+            //     maxObj[max].push([subIndxstart, subIndxend]);
+            // }
+        }
+    }
+    if (!maxObj[max]) return [[], 0];
+    for (let indexes of maxObj[max]) {
+        let [a, b] = indexes;
+        subArr.push(arr.slice(a, b + 1));
+    }
+    subArr.length > 1 ? totalArr.push(subArr) : totalArr.push(...subArr);
+    totalArr.push(max);
+    return totalArr;
+}
+
+// console.log(find_subarr_maxsum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+console.log(find_subarr_maxsum([-4, -1, -2, -1, -40, -1, -2, -1, -4]));
+// console.log(find_subarr_maxsum([4, -1, 2, 1, -40, 1, 2, -1, 4]));
