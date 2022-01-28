c919426876d2a29c72bd91976d06585fb6a46d36
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-28 11:53:07 +0300

    add challange

--- script.js
+++ script.js
@@ -136,3 +136,67 @@ function find_subarr_maxsum(arr) {
 // console.log(find_subarr_maxsum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 console.log(find_subarr_maxsum([-4, -1, -2, -1, -40, -1, -2, -1, -4]));
 // console.log(find_subarr_maxsum([4, -1, 2, 1, -40, 1, 2, -1, 4]));
+
+// 85. Find the Longest Substring Consisting of Unique Characters 6kyu
+/* Description:
+Task
+You are given a string s. It's a string consist of letters, numbers or symbols.
+
+Your task is to find the Longest substring consisting of unique characters in s, and return the length of it.
+
+Note
+1 <= s.length <= 10^7
+
+5 fixed testcases
+
+100 random testcases, testing for correctness of solution
+
+100 random testcases, testing for performance of code
+
+All inputs are valid.
+
+Pay attention to code performance.
+
+If my reference solution gives the wrong result in the random tests, please let me know(post an issue).
+
+Example
+For s="baacab", the output should be 3.
+
+The non repeating substrings in s are:
+
+"b","c","a","ba","ac","ca","ab","cab"
+The longest one is "cab", its length is 3.
+
+For s="abcd", the output should be 4.
+
+The longest one is "abcd", its length is 4.
+
+For s="!@#$%^&^%$#@!", the output should be 7.
+
+The longest substring are "!@#$%^&" and "&^%$#@!", their length both are 7. */
+
+function findLongestSubstring(str) {
+    const lookup = {};
+    let length = 0;
+    let longest = 0;
+    let lastDoubleIdx = -1;
+    for (let i = 0; i < str.length; i++) {
+        if (!(lookup[str[i]] || lookup[str[i]] === 0)) {
+            length++;
+            lookup[str[i]] = i + 1;
+        } else {
+            if (lastDoubleIdx < lookup[str[i]]) {
+                lastDoubleIdx = lookup[str[i]];
+                length = i - lastDoubleIdx;
+            }
+            lookup[str[i]] = i + 1;
+            length++;
+        }
+        if (length > longest) {
+            longest = length;
+        }
+        // console.log(str[i], length);
+        // console.log(lookup);
+    }
+    return longest;
+}
