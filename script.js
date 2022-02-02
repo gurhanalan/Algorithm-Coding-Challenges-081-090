9ef21da484f1d91b3ce6a9ecdd1d95b6315e3c1d
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-02-02 11:59:16 +0300

    add challange

--- script.js
+++ script.js
@@ -329,3 +329,26 @@ function createPhoneNumber(numbers) {
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// 90. Simple Sentences - 6kyu - codewars
+/* Implement a function, so it will produce a sentence out of the given parts.
+
+Array of parts could contain:
+
+words;
+commas in the middle;
+multiple periods at the end.
+Sentence making rules:
+
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+Example:
+
+makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.' */
+
+function makeSentence(parts) {
+    return parts.join(" ").replace(/ ,/g, ",").replace(/\./g, "").trim() + ".";
+}
+
+console.log(makeSentence(["hello", ",", "my", "dear"]));
